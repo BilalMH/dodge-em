804d7f0c06b524b772c224375e5cc92fcff15cc6
Bilal Haider <bhdevelopment92@gmail.com>
2021-12-12 18:13:15 +0000

    Created Title Screen and score board

--- main/Main.gd
+++ main/Main.gd
@@ -5,18 +5,20 @@ var score
 
 func _ready():
 	randomize()
-	new_game()
 
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 
 func _on_MobTimer_timeout():
@@ -37,10 +39,12 @@ func _on_MobTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
 
-
+func _on_HUD_start_game():
+	new_game()
